d38feb210b87fd9b3951ab6163d0429436aa707b
LucasBlasquez <42274899+LucasBlasquez@users.noreply.github.com>
2018-08-10 11:33:39 -0300

    Update responder.cs

--- Play Brain/Assets/Scripts/responder.cs
+++ Play Brain/Assets/Scripts/responder.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System;
-
+// texera
 public class responder : MonoBehaviour
 {
     public GameObject btnAvancar;
